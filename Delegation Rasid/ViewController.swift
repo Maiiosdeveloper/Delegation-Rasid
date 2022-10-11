fd1a4d1c13f5cde64425e6157dc26d84f36e6a4f
mai_ios_developer <maiaboelmaaty.fintech@gmail.com>
2022-10-11 10:12:34 +0200

    Intialize project

---
+++ Delegation Rasid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delegation Rasid
+//
+//  Created by fintech on 11/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
